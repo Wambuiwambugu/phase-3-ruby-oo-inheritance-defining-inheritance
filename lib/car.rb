ba9e80081e65da583aa91ec0515e36668f3635d0
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-07-18 23:21:12 +0300

    Done

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,10 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+    def go
+      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+  end
+
+  vanessa = Car.new(45,67)
+  puts vanessa.wheel_size
